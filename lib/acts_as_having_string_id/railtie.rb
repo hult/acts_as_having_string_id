0187a440ec223b6a43b4bd372d5e8e6b53c05944
Magnus Hult <magnus@magnushult.se>
2016-09-07 14:33:01 +0200

    Include module direclty even if spring is present

--- lib/acts_as_having_string_id/railtie.rb
+++ lib/acts_as_having_string_id/railtie.rb
@@ -1,13 +1,13 @@
 module ActsAsHavingStringId
   class Railtie < Rails::Railtie
     initializer "railtie.include_in_application_record" do
+      ApplicationRecord.include(ActsAsHavingStringId)
+
       if defined?(Spring)
         Spring.after_fork do
           # This needs to happen every time Spring reloads
           ApplicationRecord.include(ActsAsHavingStringId)
         end
-      else
-        ApplicationRecord.include(ActsAsHavingStringId)
       end
     end
   end
