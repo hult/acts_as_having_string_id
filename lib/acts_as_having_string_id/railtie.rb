3cbe37ddfc5dbc75c820abd1aad4854e3caf592e
Magnus Hult <magnus@magnushult.se>
2016-09-08 10:14:32 +0200

    Try this hook instead (#6)

--- lib/acts_as_having_string_id/railtie.rb
+++ lib/acts_as_having_string_id/railtie.rb
@@ -1,13 +1,8 @@
 module ActsAsHavingStringId
   class Railtie < Rails::Railtie
-    initializer "railtie.include_in_application_record" do
-      ApplicationRecord.include(ActsAsHavingStringId)
-
-      if defined?(Spring)
-        Spring.after_fork do
-          # This needs to happen every time Spring reloads
-          ApplicationRecord.include(ActsAsHavingStringId)
-        end
+    config.to_prepare do
+      unless ApplicationRecord.included_modules.include?(ActsAsHavingStringId)
+        ApplicationRecord.include(ActsAsHavingStringId)
       end
     end
   end
