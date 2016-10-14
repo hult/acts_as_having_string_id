795ee6caf9a51f6726884461f7b54202f2462c62
Magnus Hult <magnus@magnushult.se>
2016-10-14 10:29:00 +0200

    Seems like this works better together with spring reloading (#11)

--- lib/acts_as_having_string_id/railtie.rb
+++ lib/acts_as_having_string_id/railtie.rb
@@ -1,8 +1,8 @@
 module ActsAsHavingStringId
   class Railtie < Rails::Railtie
     config.to_prepare do
-      unless ApplicationRecord.included_modules.include?(ActsAsHavingStringId)
-        ApplicationRecord.include(ActsAsHavingStringId)
+      unless ActiveRecord::Base.included_modules.include?(ActsAsHavingStringId)
+        ActiveRecord::Base.include(ActsAsHavingStringId)
       end
     end
   end
